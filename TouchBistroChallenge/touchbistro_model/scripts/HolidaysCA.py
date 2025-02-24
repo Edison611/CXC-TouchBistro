045bd5007c46e3a21b49a508df4624b518c955af
Edison Ying <edisony611@gmail.com>
2025-02-24 01:44:29 -0500

    final commit

--- TouchBistroChallenge/touchbistro_model/scripts/HolidaysCA.py
+++ TouchBistroChallenge/touchbistro_model/scripts/HolidaysCA.py
@@ -2,14 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv("bills.csv", usecols=["business_date", "bill_total_net"])
-
+f = pd.read_csv("bills.csv")
+#print(f.head(10))
+d = pd.read_csv("venues.csv")
+#print(df.head(10))
+df = pd.merge(f, d, on="venue_xref_id", how="inner")
+df = df[["bill_total_net", "business_date", "country"]]
 
 df['business_date'] = pd.to_datetime(df['business_date'], format='%Y-%m-%d')
 
 def locateSumHoliday(date):
     
-    y = df.loc[df["business_date"] == date]
+    mask = (df["business_date"] == date) & (df["country"] == "CA")
+    
+    y = df.loc[mask]
     
     return y["bill_total_net"].sum()
 
@@ -32,7 +38,7 @@ holidays = ["canadaDay", "civicHoliday", "labourDay", "thanksgiving", "remembran
 nets =  [canadaDay, civicHoliday, labourDay, thanksgiving, remembranceDay, christmas, boxingDay]
 
 plt.bar(holidays, nets)
-plt.title('Bill total net')
+plt.title('Bill total net holidays in canada')
 plt.xlabel('Holidays')
 plt.ylabel('Money')
 plt.show()
--- TouchBistroChallenge/touchbistro_model/touchbistro_model.py
+++ TouchBistroChallenge/touchbistro_model/touchbistro_model.py
@@ -7,17 +7,11 @@ model = joblib.load("touchbistro_model/model.pkl")
 # New input data (EXCLUDING the transaction count)
 input_data = pd.DataFrame([{
     "concept_BAR": True,  # Replace with actual restaurant concept
-    "bill_day_of_week": 5,  # 0=Monday, ..., 6=Sunday
-    "date": "2025",  # Actual date
-    "bill_hour": 20  # 10 PM
+    "bill_day_of_week": 4,  # 0=Monday, ..., 6=Sunday
+    "date": "2025-01-01",  # Actual date, future concept
+    "bill_hour": 22  # 10 PM
 }])
 
-# # Convert date
-# new_data["bill_date"] = pd.to_datetime(new_data["date"])
-# new_data["month"] = new_data["bill_date"].dt.month
-# new_data["day"] = new_data["bill_date"].dt.day
-# Drop date column
-# new_data.drop(columns=["date", "bill_date"], inplace=True)
 
 # Ensure all missing columns are present
 missing_cols = [col for col in model.feature_names_in_ if col not in input_data.columns]
@@ -26,10 +20,8 @@ for col in missing_cols:
 
 # Reorder columns to match training data
 input_data = input_data[model.feature_names_in_]
-
 print(input_data)
 
 # Predict
 predicted_transactions = model.predict(input_data)
-print(predicted_transactions)
 print(f"Predicted Transactions: {predicted_transactions[0]}")
