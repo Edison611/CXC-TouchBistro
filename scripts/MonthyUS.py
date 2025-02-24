3365511b2effacad6687b83b18a87cae36020e56
Alan Fang <72469393+Alanfy1234@users.noreply.github.com>
2025-02-23 22:04:16 -0500

    Update MonthyUS.py

--- scripts/MonthyUS.py
+++ scripts/MonthyUS.py
@@ -20,19 +20,26 @@ def locateSum(startDate,endDate):
 
 
 julNet2024 = locateSum('2024-07-01', '2024-07-31') 
-augNet2024 = locateSum('2024-08-01', '2024-08-30') 
+augNet2024 = locateSum('2024-08-01', '2024-08-31') 
 sepNet2024 = locateSum('2024-09-01', '2024-09-30') 
 octNet2024 = locateSum('2024-10-01', '2024-10-31') 
-novNet2024 = locateSum('2024-11-01', '2024-11-29') 
+novNet2024 = locateSum('2024-11-01', '2024-11-30') 
 decNet2024 = locateSum('2024-12-01', '2024-12-31') 
-
 
 months = [ "jul2024", "aug2024","sep2024", "oct2024", "nov2024", "dec2024"]
 
 nets = [ julNet2024, augNet2024, sepNet2024, octNet2024, novNet2024, decNet2024]
 
+aves = [ julNet2024/31, augNet2024/31, sepNet2024/30, octNet2024/31, novNet2024/30, decNet2024/31]
+
 plt.bar(months, nets)
 plt.title('Bill total net')
 plt.xlabel('Months (USA)')
 plt.ylabel('Money')
+plt.show()
+
+plt.bar(months, aves)
+plt.title('Bill total net per day (By month)')
+plt.xlabel('Months (USA)')
+plt.ylabel('Money/day')
 plt.show()
